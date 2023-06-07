dc04e193943fdbd6240d8c66940dbba07d1360e7
Fred Baube <fbaube@welho.com>
2023-06-07 21:48:37 +0300

    Add better docu

--- logger.go
+++ logger.go
@@ -25,6 +25,32 @@ const (
 	GreenBG
 )
 
+/* NOTE also
+fromm the new package log/slog:
+const (
+	LevelDebug Level = -4
+	LevelInfo  Level = 0
+	LevelWarn  Level = 4
+	LevelError Level = 8
+)
+We wanted to make it easy to use levels to specify logger verbosity.
+Since a larger level means a more severe event, a logger that accepts
+events with smaller (or more negative) level means a more verbose log-
+ger. Logger verbosity is thus the negation of event severity, and the
+default verbosity of 0 accepts all events at least as severe as INFO.
+
+Also we wanted some room between levels to accommodate schemes with
+named levels between ours. For example, Google Cloud Logging defines
+a Notice level between Info and Warn. Since there are only a few of
+these intermediate levels, the gap between the numbers need not be
+large. Our gap of 4 matches OpenTelemetry's mapping. Subtracting 9
+from an OpenTelemetry level in the DEBUG, INFO, WARN and ERROR rang-
+es converts it to the corresponding slog Level range. OpenTelemetry
+also has the names TRACE and FATAL, which slog does not. But those
+OpenTelemetry levels can still be represented as slog Levels by
+using the appropriate integers.
+*/
+
 // LevelNames maps log levels to user-frenly names
 var LevelNames = map[Level]string{
 	LevelDbg:      "?Dbg",
